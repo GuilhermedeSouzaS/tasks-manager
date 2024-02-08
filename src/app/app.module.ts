37ba23f73b8f524237ba4fa0f55a18bd6aa6d572
GuilhermeSilva <114371029+GuilhermedeSouzaS@users.noreply.github.com>
2024-02-08 20:38:11 -0300

    (develop): upgrade login component and create sign in component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent, },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registre-se', component: SignUpComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule,
